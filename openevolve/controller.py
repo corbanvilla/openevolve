64a059d6d275eb6af1aa1d259ee95a9833ef162f
Corban Villa <corbanvilla@gmail.com>
2025-10-22 12:08:53 -0700

    initial combined score

--- openevolve/controller.py
+++ openevolve/controller.py
@@ -268,7 +268,7 @@ class OpenEvolve:
             # initial_metrics = await self.evaluator.evaluate_program(
             #     self.initial_program_code, initial_program_id
             # )
-            initial_metrics = {"score": 0}
+            initial_metrics = {"score": 0, "combined_score": 0}
 
             initial_program = Program(
                 id=initial_program_id,
